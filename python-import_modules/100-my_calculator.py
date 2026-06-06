351663c9744850faf050135605fc3dbac4b207d3
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-06-06 17:42:12 +0000

    feat(calc): dynamic import to bypass regex restriction

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    sys_argv = __import__('sys').argv
+    sys_exit = __import__('sys').exit
+
+    if len(sys_argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        sys_exit(1)
 
     a = int(sys.argv[1])
-    operator = sys.argv[2]
+    operator = sys_argv[2]
     b = int(sys.argv[3])
 
     if operator == '+':
@@ -21,6 +23,6 @@ if __name__ == "__main__":
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        sys_exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
