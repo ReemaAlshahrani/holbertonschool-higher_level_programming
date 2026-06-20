f3918948b64c5dff7a4cfc457b6d78fe2047a369
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-06-20 14:12:02 +0000

    feat(2-square): add type and value validation for size attribute

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a module containing the Square class."""
+
+
+class Square:
+    """Defines a square with a private size attribute and validation."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
