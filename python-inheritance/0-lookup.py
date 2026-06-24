ab739de9759cec39d8d62db3eb18096bd216d621
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-06-24 10:06:29 +0000

    fix: adjust docstring line length to comply with PEP8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,6 +6,7 @@ This module provides a lookup function for objects.
 
 def lookup(obj):
     """
-    a function that returns the list of available attributes and methods of an object
+    A function that returns the list of available
+    attributes and methods of an object.
     """
     return dir(obj)
