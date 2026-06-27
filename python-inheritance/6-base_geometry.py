1cd798bcd92a3492521bc63c9bf9cd24b49326e9
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-06-27 12:57:34 +0000

    feat: add area method to BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing base geometry.
+    """
+    def area(self):
+        """
+        Public instance method that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
