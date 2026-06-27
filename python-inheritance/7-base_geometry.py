d768dbba6f710469bfcac9588c4ff4001e09b848
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-06-27 13:48:52 +0000

    fix: set default value for value parameter to handle single argument calls

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry:
         """
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name, value=None):
         """Validates if value is a positive integer."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
