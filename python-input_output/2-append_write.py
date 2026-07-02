0b2826aaa4b765d14b8cf0c928d64800e12a5f9f
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-07-02 10:42:01 +0000

    style: fix line too long in append_write docstring

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,6 @@ This module contains a function that appends a string to a text file.
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a UTF-8 text file and returns chars added."""
+    """Appends a string to a UTF-8 text file and returns chars added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
