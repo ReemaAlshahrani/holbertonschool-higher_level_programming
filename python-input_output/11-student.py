c5632cdaf31889cd6fcc8273ea624de579107b5b
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-07-04 15:23:35 +0000

    feat: add reload_from_json method to Student class

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with serialization and deserialization.
+"""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attributes contained in this list
+        are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        self.__dict__.update(json)
