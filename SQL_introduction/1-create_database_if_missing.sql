7f48e850c8308ae2225649753c58815d5670d63d
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-07-19 11:26:36 +0000

    feat(sql): script that creates the database hbtn_0c_0 in your MySQL server

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Creates the database hbtn_0c_0. 
+-- The 'IF NOT EXISTS' clause prevents the script from failing 
+-- if the database is already present in the server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
