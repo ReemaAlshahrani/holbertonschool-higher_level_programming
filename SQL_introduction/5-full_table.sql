a18de708fdb70cfde131f8f7390ea7c9f9d58fa5
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-07-22 08:14:16 +0000

    feat(sql): get table description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Retrieves the full table definition (CREATE TABLE statement) 
--- from the system metadata without using restricted commands.
-SELECT CREATE_TABLE, TABLE_NAME 
-FROM information_schema.TABLES 
-WHERE TABLE_NAME = 'first_table';
+-- Retrieves the full description of the table 'first_table' 
+SHOW CREATE TABLE first_table;
