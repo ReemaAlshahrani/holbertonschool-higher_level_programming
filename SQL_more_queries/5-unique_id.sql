9aaa1b4bfe7f2881f672854292362df99525fe0d
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-07-25 15:30:50 +0000

    feat(sql): add script to create table unique_id with unique and default constraints

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- A script that creates the table unique_id on your MySQL server with a unique and default id.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
