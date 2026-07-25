c8ce58b7e500778727a22eef55a763834cee96d6
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-07-25 18:49:44 +0000

    fix(sql): correct table name to tv_genres in count shows script

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,10 +3,10 @@
 -- First column must be called genre
 -- Second column must be called number_of_shows
 -- Don't display a genre that doesn't have any shows linked
--- Results must be sorted in descending order by the number of shows linked
+-- Results must be sorted in descending order by the number_of_shows
 
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
