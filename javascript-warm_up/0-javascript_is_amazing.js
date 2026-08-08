c7a8d8c2b1cca6b817b54a9dc40dd7c018d38b64
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-08 18:38:26 +0000

    fix(javascript-warm_up): remove trailing spaces for semistandard compliance

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-/**
- * Task 0: First constant, first print
- * This script creates a constant variable 'myVar' 
- * and prints its string value to the console.
- */
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
