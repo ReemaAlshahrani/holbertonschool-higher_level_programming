26c972d2e15d6e5b55b93b5bd94ef6769857ad5e
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-10 10:31:15 +0000

    feat(python-exceptions): raise a NameError exception with a custom message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Raises a NameError exception with a custom message.
+"""
+
+
+def raise_exception_msg(message=""):
+    """Raises a NameError with a given message."""
+    raise NameError(message)
