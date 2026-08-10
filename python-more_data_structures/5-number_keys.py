0d736e77e29ba94cc0f9362b75eefdda65eac2b4
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-10 09:44:00 +0000

    feat(python-more_data_structures): add number_keys function

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary."""
+    # Use len() function to count the number of keys in the dictionary
+    return len(a_dictionary)
