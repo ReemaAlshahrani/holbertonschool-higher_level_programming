a31b2d29f2723b70c8b658c8392cb91a6b4e7072
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-12 19:38:38 +0000

    fix(tdd): pass arguments directly to np.matmul to correctly raise scalar ValueError

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -8,12 +8,5 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies 2 matrices by using the module NumPy.
-
-    Args:
-        m_a (list of lists): The first matrix (int or float).
-        m_b (list of lists): The second matrix (int or float).
-
-    Returns:
-        ndarray: The matrix product of m_a and m_b.
     """
     return np.matmul(m_a, m_b)
