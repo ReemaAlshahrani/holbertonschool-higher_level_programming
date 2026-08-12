a7f762d4b11f1b4fdace77c645ce5f7e2bf60095
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-12 20:03:31 +0000

    Fix lazy_matrix_mul implementation

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""
-Module that multiplies 2 matrices using the NumPy module.
-"""
+"""Module for lazy_matrix_mul function."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """Multiplies 2 matrices by using NumPy.
+
+    Args:
+        m_a: first matrix
+        m_b: second matrix
+
+    Returns:
+        The matrix multiplication result.
     """
-    Multiplies 2 matrices by using the module NumPy.
-    """
+    if not isinstance(m_a, (list, np.ndarray)):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, (list, np.ndarray)):
+        raise TypeError("m_b must be a list")
+
     return np.matmul(m_a, m_b)
