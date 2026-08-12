19b767451fa59edbcdf1a1446b4ad6be8a492751
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-12 20:20:18 +0000

    Use simple np.matmul implementation

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,19 +4,5 @@ import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies 2 matrices by using NumPy.
-
-    Args:
-        m_a: first matrix
-        m_b: second matrix
-
-    Returns:
-        The matrix multiplication result.
-    """
-    if isinstance(m_a, (str, int, float, bool)) or isinstance(m_b, (str, int, float, bool)):
-        raise ValueError("Scalar operands are not allowed, use '*' instead")
-
-    try:
-        return np.matmul(m_a, m_b)
-    except TypeError as e:
-        raise TypeError(str(e))
+    """Multiplies 2 matrices by using NumPy."""
+    return np.matmul(m_a, m_b)
