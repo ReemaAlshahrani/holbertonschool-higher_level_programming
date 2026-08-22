273408effe7e295378ccccaf886d5693556b4a55
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-22 17:34:30 +0000

    feat(Python): add docstrings to LockedClass for style compliance

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
+
+
 class LockedClass:
+    """A class that prevents the user from dynamically creating
+    new instance attributes, except if the attribute is first_name.
+    """
     __slots__ = ["first_name"]
