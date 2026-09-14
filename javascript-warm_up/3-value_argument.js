9001cedb1a133c749c763e7d422aea5025d63f15
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-14 19:20:27 +0300

    feat(javascript-warm_up): print the first argument or No argument without using length

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+
+// Check if the first argument (index 2) is undefined (no arguments passed)
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
+  // Print the first argument passed to the script
   console.log(process.argv[2]);
 }
