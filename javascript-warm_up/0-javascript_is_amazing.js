130772250b4417bbfb6a724a7d8ce56995156b71
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-14 17:50:40 +0300

    feat(javascript-warm_up): add script to print a constant string

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
+
+// Define a constant variable 'myVar' with the string value
 const myVar = 'JavaScript is amazing';
+
+// Print the value stored in myVar to the console
 console.log(myVar);
