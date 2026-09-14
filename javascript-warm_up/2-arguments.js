dcf29ada1216a968c4b02ebd911398064d836948
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-14 18:39:25 +0300

    feat(javascript-warm_up): add script to print message based on number of arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
+
+// Calculate the number of actual arguments by subtracting Node.js and script paths
 const args = process.argv.length - 2;
 
+// Check if no arguments were passed
 if (args === 0) {
   console.log('No argument');
-} else if (args === 1) {
+} 
+// Check if exactly one argument was passed
+else if (args === 1) {
   console.log('Argument found');
-} else {
+} 
+// Handle the case when multiple arguments were passed
+else {
   console.log('Arguments found');
 }
