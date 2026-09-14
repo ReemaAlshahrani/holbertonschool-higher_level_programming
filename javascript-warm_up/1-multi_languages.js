ad8adb8feca8f57f4cc32855c1775b0dea597772
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-14 18:04:31 +0300

    feat(javascript-warm_up): add script to print 3 lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+
+// Print the first line stating C is fun
 console.log('C is fun');
+
+// Print the second line stating Python is cool
 console.log('Python is cool');
+
+// Print the third line stating JavaScript is amazing
 console.log('JavaScript is amazing');
