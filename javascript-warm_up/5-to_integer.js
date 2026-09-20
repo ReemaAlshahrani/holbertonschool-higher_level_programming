9c5ffe78bd344441d2fe02659383d3b99bd12d4d
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-20 18:14:53 +0300

    feat(javascript-basics): add validation for numeric command-line arguments

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+
+// Convert the first command-line argument to a number and floor it
 const num = Math.floor(Number(process.argv[2]));
 
+// Check if the resulting value is NaN (Not a Number)
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
+  // If it is a valid number, print it with the required format
   console.log('My number: ' + num);
 }
