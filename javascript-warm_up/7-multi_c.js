2369c0d9c9ee35b8edf267d8e5246d169637395f
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-20 18:47:06 +0300

    feat(javascript-warm_up): add script to print 'C is fun' x times using a loop

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+
+// Convert the command line argument to an integer representing the number of occurrences
 const x = Math.floor(Number(process.argv[2]));
 
+// Check if the input is not a valid number
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
+  // Loop 'x' times and print the message in each iteration
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
