0f1ad9fb24c24826f222dd8fcb7a0a4ffe80d70a
Reema Ali Alshahrani <reema.alshahrani.ali@gmail.com>
2026-09-21 13:45:37 +0300

    refactor(javascript): optimize factorial function using recursion and handle base cases

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
+
+//Calculates the factorial of a given number recursively.
 function factorial (n) {
+  // Base case: return 1 if n is not a valid number or less than or equal to 1
   if (isNaN(n) || n <= 1) {
     return 1;
   }
+  
+  // Recursive step: multiply n by the factorial of (n - 1)
   return n * factorial(n - 1);
 }
 
+// Convert the command-line argument to a number
 const arg = Number(process.argv[2]);
+
+// Print the final result of the factorial calculation
 console.log(factorial(arg));
